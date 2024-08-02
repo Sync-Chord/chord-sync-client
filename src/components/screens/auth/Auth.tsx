974c88ef222650b9d5f1493b90af42addffec4f1
theHarshRohit <harshrohit0700@gmail.com>
2024-08-02 20:45:29 +0530

    Added Authentication Pages

---
+++ src/components/screens/auth/Auth.tsx
@@ -0,0 +1,30 @@
+import { lazy } from "react"
+import { Routes, Route, Navigate } from "react-router-dom"
+import Otp from "./Otp"
+import { ToastContainer } from "react-toastify"
+
+const Login = lazy(() => import("./Login"))
+const Signup = lazy(() => import("./SignUp"))
+
+const Auth = () => {
+  return (
+    <>
+      <ToastContainer
+        pauseOnFocusLoss={false}
+        position="top-center"
+        pauseOnHover={false}
+        autoClose={2000}
+      />
+
+      <Routes>
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/auth/login" />} />
+        <Route path="login-by-otp" element={<Otp type="login" />} />
+        <Route path="forget-password" element={<Otp type="forgetpassword" />} />
+      </Routes>
+    </>
+  )
+}
+
+export default Auth
--- src/components/screens/auth/SignUp.tsx
+++ src/components/screens/auth/SignUp.tsx
@@ -4,26 +4,25 @@ import Button from "@mui/material/Button"
 import CssBaseline from "@mui/material/CssBaseline"
 import TextField from "@mui/material/TextField"
 import Link from "@mui/material/Link"
-import Paper from "@mui/material/Paper"
-import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
+import Box from "@mui/material/Box"
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
 import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 import { useNavigate } from "react-router-dom"
-import { Container } from "@mui/system"
+import Paper from "@mui/material/Paper"
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme()
 
-export default function Login() {
+export default function SignUp() {
   const nav = useNavigate()
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const data = new FormData(event.currentTarget)
     console.log({
-      email: data.get("email"),
+      uniqueId: data.get("uniqueId"),
       password: data.get("password"),
     })
   }
@@ -62,34 +61,48 @@ export default function Login() {
                 <LockOutlinedIcon />
               </Avatar>
               <Typography component="h1" variant="h5">
-                Sign in
+                Sign up
               </Typography>
               <Box
                 component="form"
                 noValidate
                 onSubmit={handleSubmit}
-                sx={{ mt: 1 }}
+                sx={{ mt: 3 }}
               >
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      autoComplete="given-name"
+                      name="name"
+                      required
+                      fullWidth
+                      id="name"
+                      label="Name"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="uniqueId"
+                      label="Email or Phone"
+                      name="uniqueId"
+                      autoComplete="uniqueId"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                    />
+                  </Grid>
+                </Grid>
                 <Button
                   type="submit"
                   fullWidth
@@ -97,49 +110,23 @@ export default function Login() {
                   sx={{ mt: 3, mb: 2 }}
                   style={{ backgroundColor: "#0C7075" }}
                 >
-                  Sign In
+                  Sign Up
                 </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <Link
-                      onClick={() => nav("/auth/forget-password")}
-                      style={{
-                        color: "#0C7075",
-                        fontSize: "15px",
-                        cursor: "pointer",
-                      }}
-                      variant="body2"
-                    >
-                      Forgot password?
-                    </Link>
-                  </Grid>
+                <Grid container justifyContent="flex-end">
                   <Grid item>
                     <Link
-                      onClick={() => nav("/auth/login-by-otp")}
+                      onClick={() => nav("/auth/login")}
                       style={{
                         color: "#0C7075",
                         fontSize: "15px",
                         cursor: "pointer",
                       }}
                     >
-                      {"Sign In with OTP"}
+                      {"  Already have an account? Sign in"}
                     </Link>
                   </Grid>
                 </Grid>
               </Box>
-
-              <Grid item style={{ padding: "20px", fontSize: "15px" }}>
-                <Link
-                  onClick={() => nav("/auth/signup")}
-                  style={{
-                    color: "#0C7075",
-                    fontSize: "15px",
-                    cursor: "pointer",
-                  }}
-                >
-                  {"Don't Have an Account ? Register Here"}
-                </Link>
-              </Grid>
             </Box>
           </Grid>
         </Box>
