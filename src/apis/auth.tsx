967978b3c360612f97a851af018116d441cf436f
Singh1289 <singh.1289abhi@gmail.com>
2024-08-03 19:48:46 +0530

    added auth api

--- src/apis/auth.tsx
+++ src/apis/auth.tsx
@@ -1,42 +1,66 @@
-import axios from "axios"
+import axios from "axios";
 // component imports
-import default_catch from "../utils/error"
+import default_catch from "../utils/error";
 
 const instance = axios.create({
   baseURL: "http://localhost:3000/auth",
-})
+});
 
 class Auth {
-  // for login 
+  // for login
   static login(payload: any) {
     return new Promise((resolve) => {
       instance
         .post("/login", payload)
         .then((response) => {
-          resolve(response)
+          resolve(response);
         })
         .catch((err) => {
-          default_catch(err, resolve)
-        })
-    })
+          default_catch(err, resolve);
+        });
+    });
   }
 
-
-  static register(payload:any){
+  // for sign up
+  static register(payload: any) {
     return new Promise((resolve) => {
       instance
         .post("/signup", payload)
         .then((response) => {
-          resolve(response)
+          resolve(response);
         })
         .catch((err) => {
-          default_catch(err, resolve)
+          default_catch(err, resolve);
+        });
+    });
+  }
+  //for forget password / change url to  and verify otp pending
+  static forgetPassword(payload: any) {
+    return new Promise((resolve) => {
+      instance
+        .post("/sign_in_by_otp", payload)
+        .then((response) => {
+          resolve(response);
         })
-    })
+        .catch((err) => {
+          default_catch(err, resolve);
+        });
+    });
   }
 
-// forget password
-// otp * 4
+  // to resend otp
+  static resendOtp(payload: any) {
+    return new Promise((resolve) => {
+      instance
+        .post("/resend_otp", payload)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((err) => {
+          default_catch(err, resolve);
+        });
+    });
+  }
 }
 
 export default Auth;
