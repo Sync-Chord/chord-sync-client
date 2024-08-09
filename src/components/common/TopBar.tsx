29b4b0bea3a304a92d67365a2cf1b6463e22cb9f
theHarshRohit <harshrohit0700@gmail.com>
2024-08-09 13:18:15 +0530

    TopBar Added

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,7 +1,194 @@
-import { Box } from "@mui/material";
+import * as React from "react"
+import { styled, alpha } from "@mui/material/styles"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import IconButton from "@mui/material/IconButton"
+import Typography from "@mui/material/Typography"
+import InputBase from "@mui/material/InputBase"
+import Badge from "@mui/material/Badge"
+import MenuItem from "@mui/material/MenuItem"
+import Menu from "@mui/material/Menu"
+import SearchIcon from "@mui/icons-material/Search"
+import AccountCircle from "@mui/icons-material/AccountCircle"
+import PeopleIcon from "@mui/icons-material/People"
+
+// Styled components
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: "#c0eaca",
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "70ch", // Adjust the width as needed
+  },
+}))
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  top: 0,
+  bottom: 0,
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "black",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+  },
+}))
 
 const TopBar = () => {
-  return <Box sx={{}}>Top bar</Box>;
-};
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    React.useState<null | HTMLElement>(null)
 
-export default TopBar;
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null)
+  }
+
+  const mobileMenuId = "primary-search-account-menu-mobile"
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+          sx={{
+            color: "black",
+            "&:hover": {
+              backgroundColor: "#27AE60",
+              color: "white",
+            },
+          }}
+        >
+          <Badge badgeContent={17} color="error">
+            <PeopleIcon />
+          </Badge>
+        </IconButton>
+        <p style={{ color: "black" }}>Notifications</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          color="inherit"
+          sx={{
+            color: "black",
+            "&:hover": {
+              backgroundColor: "#27AE60",
+              color: "white",
+            },
+          }}
+        >
+          <AccountCircle />
+        </IconButton>
+        <p style={{ color: "black" }}>Profile</p>
+      </MenuItem>
+    </Menu>
+  )
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "white", boxShadow: "none", border: "none" }}
+      >
+        <Toolbar>
+          <Box sx={{ display: { xs: "none", sm: "block" }, flexGrow: 1 }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{
+                color: "#27AE60",
+                fontWeight: "700",
+              }}
+            >
+              SYNC
+            </Typography>
+          </Box>
+          <Box
+            sx={{ flexGrow: 1, display: "flex", justifyContent: "flex-start" }}
+          >
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon
+                  sx={{ color: "black", "&:hover": { color: "#27AE60" } }}
+                />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search Music…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Box>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              color="inherit"
+              sx={{
+                color: "black",
+                "&:hover": {
+                  backgroundColor: "#27AE60",
+                  color: "white",
+                },
+              }}
+            >
+              <AccountCircle />
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+              sx={{
+                color: "black",
+                "&:hover": {
+                  backgroundColor: "#27AE60",
+                  color: "white",
+                },
+              }}
+            >
+              <Badge badgeContent={17} color="error">
+                <PeopleIcon />
+              </Badge>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+    </Box>
+  )
+}
+
+export default TopBar
