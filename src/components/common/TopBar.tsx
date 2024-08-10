f1a09a1e0a2acf921eee7febe26117684125b4f3
theHarshRohit <harshrohit0700@gmail.com>
2024-08-10 17:31:07 +0530

    Friend Request Added as Menu

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,21 +1,23 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import PeopleIcon from "@mui/icons-material/People";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import "../../assests/images/symbol.jpg";
-import { Avatar } from "@mui/material";
+import { useState } from "react"
+import { styled } from "@mui/material/styles"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import IconButton from "@mui/material/IconButton"
+import Typography from "@mui/material/Typography"
+import InputBase from "@mui/material/InputBase"
+import Badge from "@mui/material/Badge"
+
+import Menu from "@mui/material/Menu"
+import SearchIcon from "@mui/icons-material/Search"
+import AccountCircle from "@mui/icons-material/AccountCircle"
+import PeopleIcon from "@mui/icons-material/People"
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import "../../assests/images/symbol.jpg"
+import { Avatar, Divider, Grid, ListItemIcon, MenuItem } from "@mui/material"
+import { Logout, PersonAdd, Settings } from "@mui/icons-material"
+import FriendsCard from "./FriendsCard"
 
 // Styled components
 const Search = styled("div")(({ theme }) => ({
@@ -29,7 +31,7 @@ const Search = styled("div")(({ theme }) => ({
     marginLeft: theme.spacing(3),
     width: "70ch", // Adjust the width as needed
   },
-}));
+}))
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
@@ -41,7 +43,7 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
   justifyContent: "center",
   top: 0,
   bottom: 0,
-}));
+}))
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "black",
@@ -52,11 +54,67 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     transition: theme.transitions.create("width"),
     width: "100%",
   },
-}));
+}))
 
 const TopBar = () => {
-  const navigate = useNavigate();
-  const { user } = useSelector((state: any) => state.auth.user);
+  const navigate = useNavigate()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  const { user } = useSelector((state: any) => state.auth.user)
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const friends = [
+    {
+      profilePhoto: "https://via.placeholder.com/50",
+      userName: "Alice Johnson",
+      joinedSince: "March 2019",
+    },
+    {
+      profilePhoto: "https://via.placeholder.com/50",
+      userName: "Bob Smith",
+      joinedSince: "June 2020",
+      status: "accepted",
+    },
+    {
+      profilePhoto: "https://via.placeholder.com/50",
+      userName: "Charlie Brown",
+      joinedSince: "August 2021",
+      status: "accepted",
+    },
+    {
+      profilePhoto: "https://via.placeholder.com/50",
+      userName: "David Williams",
+      joinedSince: "December 2022",
+      status: "accepted",
+    },
+    {
+      profilePhoto: "https://via.placeholder.com/50",
+      userName: "Eve Davis",
+      joinedSince: "January 2021",
+      status: "accepted",
+    },
+    {
+      profilePhoto: "https://via.placeholder.com/50",
+      userName: "David Williams",
+      joinedSince: "December 2022",
+    },
+    {
+      profilePhoto: "https://via.placeholder.com/50",
+      userName: "Eve Davis",
+      joinedSince: "January 2021",
+    },
+  ]
+
+  const handleAddFriend = () => {
+    console.log("heyyy")
+    //nav("/friend");
+  }
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -68,7 +126,9 @@ const TopBar = () => {
           <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
             <Search>
               <SearchIconWrapper>
-                <SearchIcon sx={{ color: "black", "&:hover": { color: "#27AE60" } }} />
+                <SearchIcon
+                  sx={{ color: "black", "&:hover": { color: "#27AE60" } }}
+                />
               </SearchIconWrapper>
               <StyledInputBase
                 placeholder="Search Music…"
@@ -82,9 +142,10 @@ const TopBar = () => {
             }}
           >
             <IconButton
-              onClick={() => {
-                navigate("/friend");
-              }}
+              onClick={handleClick}
+              aria-controls={open ? "account-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
               size="large"
               aria-label="show 17 new notifications"
               color="inherit"
@@ -100,7 +161,7 @@ const TopBar = () => {
             <IconButton
               size="large"
               onClick={() => {
-                navigate("/profile");
+                navigate("/profile")
               }}
               edge="end"
               aria-label="account of current user"
@@ -111,12 +172,85 @@ const TopBar = () => {
             >
               <Avatar sx={{ bgcolor: "#27AE60" }}>{user.name[0]}</Avatar>
             </IconButton>
-            <Typography sx={{ color: "black", alignItems: "center" }}>{user.name}</Typography>
+            <Typography sx={{ color: "black", alignItems: "center" }}>
+              {user.name}
+            </Typography>
           </Box>
         </Toolbar>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: "visible",
+              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+              mt: 1.5,
+              "& .MuiAvatar-root": {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              "&::before": {
+                content: '""',
+                display: "block",
+                position: "absolute",
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: "background.paper",
+                transform: "translateY(-50%) rotate(45deg)",
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <Grid
+            item
+            xs={6}
+            sx={{
+              height: "80vh",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Grid sx={{ height: "5rem" }}>
+              <Typography
+                component="div"
+                sx={{
+                  textAlign: "center",
+                  fontWeight: "600",
+                  fontSize: "25px",
+                  color: "black",
+                }}
+              >
+                Friend Requests
+              </Typography>
+            </Grid>
+            <Grid sx={{ overflow: "auto", scrollbarWidth: "none" }}>
+              {friends.map((friend, index) => (
+                <FriendsCard
+                  key={index}
+                  profilePhoto={friend.profilePhoto}
+                  userName={friend.userName}
+                  joinedSince={friend.joinedSince}
+                  onAddFriend={handleAddFriend}
+                  type="friend"
+                />
+              ))}
+            </Grid>
+          </Grid>
+        </Menu>
       </AppBar>
     </Box>
-  );
-};
+  )
+}
 
-export default TopBar;
+export default TopBar
